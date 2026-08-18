938e021c8d004cd1cfbae5755ab823c812b2a9a1
Rafael S. <kaitoharu6in6@gmail.com>
2026-08-18 20:39:46 -0300

    Aula de Entradas 18/08
    Aula sobre Entrada e NPM

---
+++ 002-entrada.js
@@ -0,0 +1,12 @@
+let entrada = require('prompt-sync')();
+
+let conteudoDigitado = entrada("Digite uma frase: ");
+
+console.log(conteudoDigitado);
+
+var entrada = require('promp-sync')({
+ history: require ('prompt-sync-history')()
+});
+
+var input = prompt()
+prompt.history.save()
