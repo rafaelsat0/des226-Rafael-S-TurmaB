c4ff5c18f8a77e50b432efa7262e436448d18e79
Rafael S. <kaitoharu6in6@gmail.com>
2026-08-25 20:37:17 -0300

    Atividade-arrays

---
+++ 004-tipos-especiais.js
@@ -0,0 +1,10 @@
+// array -> Lista ou Matriz de valores
+
+let listaTimes = ['RedBull Recing', 'Mercedes', 'Mc Laren',
+     'Audi', 'Willians' ];
+     
+     let listaMegaSena = [1, 21, 23, 31, 45, 60];
+
+     let data = new Date();
+
+     console.log(data);
